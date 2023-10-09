e688c314f50d1ecb307fd5f99cc4d7fe8028d3a6
craftidev <ivann.codev@gmail.com>
2023-10-09 12:39:29 +0200

    add exercise 1b sorting a string alphabetically

--- JavaScript/exercise1b.js
+++ JavaScript/exercise1b.js
@@ -1,1 +1,9 @@
-function exercise1b() {}
+function exercise1b() {
+    const START = "une chaine avec des lettres dans un certain ordre pour donner du sens";
+    let result = START.replace(/\s+/g, '').split('').sort();
+
+    return "Starting with the string:<br />" +
+        START +
+        "<br />And sorted alphabetically:<br />" +
+        result;
+}
