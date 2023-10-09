5bcb076c1e7a3880ae1c746ac301d49d284ea090
craftidev <ivann.codev@gmail.com>
2023-10-09 13:09:21 +0200

    add last exercise: putting first letters in uppercase

--- JavaScript/exercise1c.js
+++ JavaScript/exercise1c.js
@@ -1,1 +1,16 @@
-function exercise1c() {}
+function exercise1c() {
+    const PHRASE = "une phrase sans majuscule";
+    let words = PHRASE.split(' ');
+    for(let word of words) {
+        let characters = word.split('');
+        let firstLetter = characters[0];
+        characters[0] = firstLetter.toUpperCase();
+        words[words.indexOf(word)] = characters.join('');
+    }
+
+    return "Starting with the string:<br />" +
+        PHRASE +
+        "<br />And putting the first letter in uppercase:<br /><strong>" +
+        words.join(' ') +
+        "</strong>";
+}
