606265a50ad7178a1a4dae251473a9f267fa5b25
craftidev <ivann.codev@gmail.com>
2023-10-09 11:11:03 +0200

    add first exercise. Count iteration to generate all number randomly in a range

---
+++ JavaScript/exercise1a.js
@@ -0,0 +1,26 @@
+function exercise1a() {
+    let rangeList = [];
+    let iterationCounter = 0;
+
+    for(let i = 0; i < 100; i++) {
+        rangeList.push(i);
+    }
+
+    while(rangeList.length > 0 || iterationCounter > 1_000_000) {
+        rangeList.splice(getRandomInt(), 1)
+
+        iterationCounter++
+    }
+
+    if (iterationCounter == 1_000_000) {
+        return "Error, the loop overflowed."
+    } else {
+        return "<strong>" +
+        iterationCounter +
+        "</strong> was the number of iteration necessary to draw all the number in the range of [0;100["
+    }
+
+    function getRandomInt(max = 99) {
+        return Math.floor(Math.random() * max);
+    }
+}
