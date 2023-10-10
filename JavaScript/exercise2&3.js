b62c4d37bfd4bfbfe9bdec4b5a9c452cb0f19019
craftidev <ivann.codev@gmail.com>
2023-10-10 13:23:19 +0200

    refactor to parse user inputs into JS Object. Overide toHTML method.

--- JavaScript/exercise2&3.js
+++ JavaScript/exercise2&3.js
@@ -1,3 +1,32 @@
+function Article(title, author, date) {
+    this.title = title;
+    this.author = author;
+    this.date = date;
+}
+
+Article.prototype.toHTML = function () {
+    let li = Object.assign(document.createElement("li"), { style: "list-style-type: decimal;" });
+    let title = document.createElement("h1");
+    title.classList.add("title-in-decimal-list");
+    let authorAndDate = document.createElement("p");
+
+    title.textContent = this.title;
+    authorAndDate.textContent = this.author + ", " + this.date;
+
+    li.appendChild(title);
+    li.appendChild(authorAndDate);
+
+    li.onclick = function() {
+        let userConfirm = confirm(
+            this.firstElementChild.textContent +
+            " ------ Do you want to delete this entry?"
+        );
+        if (userConfirm === true) {
+            displayResult.removeChild(this);
+        }
+    }
+}
+
 function exercise2_3() {
     let result = document.createElement("div")
     let form = document.createElement("form");
@@ -7,13 +36,20 @@ function exercise2_3() {
     let displayResult = document.createElement("ul");
 
     // Form elements
-    let labelTitle = Object.assign(document.createElement("label"), { type: "text", for: "title" });
-    let inputTitle = Object.assign(document.createElement("input"), { type: "text", id: "title", name: "title", required: true });
-    let labelAuthor = Object.assign(document.createElement("label"), { for: "author" });
-    let inputAuthor = Object.assign(document.createElement("input"), { type: "text", id: "author", name: "author", required: true });
-    let labelDate = Object.assign(document.createElement("label"), { for: "date" });
-    let inputDate = Object.assign(document.createElement("input"), { type: "date", id: "date", name: "date", required: true });
-    let inputSubmit = Object.assign(document.createElement("input"), { type: "submit" });
+    let labelTitle = Object.assign(document.createElement("label"),
+        { type: "text", for: "title" });
+    let inputTitle = Object.assign(document.createElement("input"),
+        { type: "text", id: "title", name: "title", required: true });
+    let labelAuthor = Object.assign(document.createElement("label"),
+        { for: "author" });
+    let inputAuthor = Object.assign(document.createElement("input"),
+        { type: "text", id: "author", name: "author", required: true });
+    let labelDate = Object.assign(document.createElement("label"),
+        { for: "date" });
+    let inputDate = Object.assign(document.createElement("input"),
+        { type: "date", id: "date", name: "date", required: true });
+    let inputSubmit = Object.assign(document.createElement("input"),
+        { type: "submit" });
 
     labelTitle.textContent = "Title: ";
     labelAuthor.textContent = "Author: ";
@@ -27,7 +63,7 @@ function exercise2_3() {
     form.appendChild(inputDate);
     form.appendChild(inputSubmit);
 
-
+    // Main event, submiting form
     form.addEventListener("submit", function(event) {
         event.preventDefault();
 
@@ -41,28 +77,8 @@ function exercise2_3() {
         }
 
         else {
-            let li = Object.assign(document.createElement("li"), { style: "list-style-type: decimal;" });
-            let title = document.createElement("h1");
-            title.classList.add("title-in-decimal-list");
-            let authorAndDate = document.createElement("p");
-
-            title.textContent = form.elements.title.value;
-            authorAndDate.textContent = form.elements.author.value + ", " + form.elements.date.value;
-
-            li.appendChild(title);
-            li.appendChild(authorAndDate);
-
-            displayResult.appendChild(li);
-
-            li.onclick = function() {
-                let userConfirm = confirm(
-                    this.firstElementChild.textContent +
-                    " ------ Do you want to delete this entry?"
-                );
-                if (userConfirm === true) {
-                    displayResult.removeChild(this);
-                }
-            }
+            const newEntry = new Article(form.elements.title.value, form.elements.author.value, form.elements.date.value);
+            displayResult.appendChild(newEntry.toHTML());
         }
     });
 
